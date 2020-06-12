ebeccda23bc81a7f314a5e98d577656646d28e2d
MMcGrane <markmcgrane@hotmail.com>
2020-06-12 20:21:33 +0100

    Updates 12/6/20
    Added a platform, fixed some images and started to work on generating random drops of fire to hit the player.

--- game.js
+++ game.js
@@ -1,22 +1,40 @@
 const gameState = {}
 
+
+
 function preload() {
-    this.load.image("Stickman", "stickman.png")
+    this.load.image("Stickman", "stickman2.png")
+    this.load.image("platform", "couches.png")
+    this.load.image("lavadrop", "lavadrop.png")
+    this.load.image("background", "background.png")
 }
 
 function create() {
+    this.add.image(400, 300, "background");
     gameState.stickman = this.physics.add.sprite(70, 50, "Stickman")
     gameState.keys = this.input.keyboard.createCursorKeys();
+    const platform = this.physics.add.staticGroup();
+    platform.create(400, 400, "platform");
+    gameState.stickman.setCollideWorldBounds(true);
+
+    this.physics.add.collider(gameState.stickman, platform);
+
+    const drop = this.physics.add.group();
+    const coordinates = Math.random() * 450;
+    var x = 1;
+    
 }
 
 function update() {
     if(gameState.keys.right.isDown) {
-        gameState.stickman.setVelocityX(180);
+        gameState.stickman.setVelocityX(160);
     }
     else if(gameState.keys.left.isDown) {
-        gameState.stickman.setVelocityX(-180);
+        gameState.stickman.setVelocityX(-160);
     }
 }
+
+
 
 const config = {
     type: Phaser.AUTO,
@@ -36,5 +54,6 @@ const config = {
         update
     }
 }
+
 
 const game = new Phaser.Game(config)
